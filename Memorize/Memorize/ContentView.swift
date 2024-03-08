4f0e31fb5e156352025244054f73c21c5f97fc8d
Oriol Angelet <uriaeri@gmail.com>
2024-03-08 18:53:55 +0100

    Initial Xcode setup

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Uri on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
