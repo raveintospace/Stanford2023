94e58e0bb91caec4a413630c7f0d999f3a300e64
Oriol Angelet <uriaeri@gmail.com>
2024-03-09 16:42:36 +0100

    ViewModel created

---
+++ Memorize/Memorize/ViewModel/EmojiMemoryGame.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Uri on 9/3/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemorizeGame<String>
+    
+    init(model: MemorizeGame<String>) {
+        self.model = model
+    }
+}
