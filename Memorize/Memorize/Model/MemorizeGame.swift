fbc90c1a94a9f26da8a1e30a514205a124334674
Oriol Angelet <uriaeri@gmail.com>
2024-03-09 16:35:10 +0100

    Model created

---
+++ Memorize/Memorize/Model/MemorizeGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Uri on 9/3/24.
+//
+
+import Foundation
+
+struct MemorizeGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
