6fd88545ddf39c8bf98e5c85e8829d39972031f3
Oriol Angelet <uriaeri@gmail.com>
2024-03-10 16:09:25 +0100

    Lesson 5 Updated choose & inde methods in Model to work with optionals

--- Memorize/Memorize/Model/MemorizeGame.swift
+++ Memorize/Memorize/Model/MemorizeGame.swift
@@ -26,17 +26,18 @@ struct MemorizeGame<CardContent> where CardContent: Equatable {
     }
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()       
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
-    func index(of card: Card) -> Int {
+    private func index(of card: Card) -> Int? {
         for index in cards.indices {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 // FIXME: bogus!
+        return nil
     }
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
