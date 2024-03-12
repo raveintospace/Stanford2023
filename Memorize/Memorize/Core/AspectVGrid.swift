a147bedadbbe552074d50d75ab5ae7f8d26f0511
Oriol Angelet <uriaeri@gmail.com>
2024-03-12 19:13:58 +0100

    Lesson 6 finished AspectVGrid view to set the items on screen according to GeometryReader

---
+++ Memorize/Memorize/Core/AspectVGrid.swift
@@ -0,0 +1,58 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Uri on 12/3/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
+    
+    var items: [Item]
+    var aspectRatio: CGFloat = 1
+    var content: (Item) -> ItemView
+    
+    init(_ items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.aspectRatio = aspectRatio
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                itemCount: items.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio
+            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(items) { item in
+                    content(item)
+                        .aspectRatio(aspectRatio, contentMode: .fit)
+                }
+            }
+        }
+    }
+    
+    // set the items on screen using the space read with GeometryReader
+    func gridItemWidthThatFits(
+        itemCount: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let itemCount = CGFloat(itemCount)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (itemCount / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < itemCount
+        return min(size.width / itemCount, size.height * aspectRatio).rounded(.down)
+    }
+}
