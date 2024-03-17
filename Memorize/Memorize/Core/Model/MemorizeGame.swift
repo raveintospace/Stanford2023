90f65933ec4b0efc0c3619d8e840a9e2a1995713
Oriol Angelet <uriaeri@gmail.com>
2024-03-17 15:54:27 +0100

    Lesson 9 Anotation with scoring explanation Copied code from repo for bonus time

--- Memorize/Memorize/Core/Model/MemorizeGame.swift
+++ Memorize/Memorize/Core/Model/MemorizeGame.swift
@@ -31,6 +31,10 @@ struct MemorizeGame<CardContent> where CardContent: Equatable {
         cards.shuffle()
     }
     
+    /* Scoring explanation
+    - 1 if one of the two cards is first time seen
+    - 2 if both cards have been seen before
+    */    
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
             if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
@@ -71,5 +75,51 @@ struct MemorizeGame<CardContent> where CardContent: Equatable {
         var debugDescription: String {
             "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
         }
+        
+        // MARK: - Bonus Time - Code from repo
+        
+        // call this when the card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isFaceUp && !isMatched && bonusPercentRemaining > 0, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        // call this when the card goes back face down or gets matched
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            lastFaceUpDate = nil
+        }
+        
+        // the bonus earned so far (one point for every second of the bonusTimeLimit that was not used)
+        // this gets smaller and smaller the longer the card remains face up without being matched
+        var bonus: Int {
+            Int(bonusTimeLimit * bonusPercentRemaining)
+        }
+        
+        // percentage of the bonus time remaining
+        var bonusPercentRemaining: Double {
+            bonusTimeLimit > 0 ? max(0, bonusTimeLimit - faceUpTime)/bonusTimeLimit : 0
+        }
+        
+        // how long this card has ever been face up and unmatched during its lifetime
+        // basically, pastFaceUpTime + time since lastFaceUpDate
+        var faceUpTime: TimeInterval {
+            if let lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // can be zero which would mean "no bonus available" for matching this card quickly
+        var bonusTimeLimit: TimeInterval = 6
+        
+        // the last time this card was turned face up
+        var lastFaceUpDate: Date?
+        
+        // the accumulated time this card was face up in the past
+        // (i.e. not including the current time it's been face up if it is currently so)
+        var pastFaceUpTime: TimeInterval = 0
     }
 }
