c52a43cd0f98bdd7249aace5d158a2070dfea3f8
Oriol Angelet <uriaeri@gmail.com>
2024-03-17 15:26:39 +0100

    Lesson 9 method scoreChanged implemented in view, reads what card updates the score

--- Memorize/Memorize/Core/View/EmojiMemorizeGameView.swift
+++ Memorize/Memorize/Core/View/EmojiMemorizeGameView.swift
@@ -11,6 +11,9 @@ struct EmojiMemorizeGameView: View {
     
     @ObservedObject var viewModel: EmojiMemoryGameViewModel
     
+    // tuple with Int & Card.Id as parameters
+    @State private var lastScoreChange = (0, causedByCardId: "")
+    
     private let cardAspectRatio: CGFloat = 2/3
     private let spacing: CGFloat = 4
     
@@ -37,14 +40,18 @@ struct EmojiMemorizeGameView: View {
                 .overlay(FlyingNumber(number: scoreChanged(causedBy: card)))
                 .onTapGesture {
                     withAnimation(.easeInOut(duration: 0.5)) {
+                        let scoreBeforeChoosing = viewModel.score
                         viewModel.choose(card)
+                        let scoreChange = viewModel.score - scoreBeforeChoosing
+                        lastScoreChange = (scoreChange, causedByCardId: card.id)
                     }
                 }
         }
     }
     
-    private func scoreChanged(causedBy: Card) -> Int {
-        return 0
+    private func scoreChanged(causedBy card: Card) -> Int {
+        let (amount, causedByCardId: id) = lastScoreChange
+        return card.id == id ? amount : 0
     }
     
     private var score: some View {
