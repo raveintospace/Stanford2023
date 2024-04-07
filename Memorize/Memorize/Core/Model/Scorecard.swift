141a31c4efcd0c9cf4bb252ba30acc6bff594a49
Oriol Angelet <uriaeri@gmail.com>
2024-04-07 12:49:36 +0200

    Scorecard model created

---
+++ Memorize/Memorize/Core/Model/Scorecard.swift
@@ -0,0 +1,15 @@
+//
+//  Scorecard.swift
+//  Memorize
+//
+//  Created by Uri on 7/4/24.
+//
+
+import Foundation
+
+struct Scorecard {
+    var player: String
+    var deck: String
+    var matches: Int
+    var score: Int
+}
