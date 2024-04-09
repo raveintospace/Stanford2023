ebc9e01f4b78238b0ad061dea1545a1cae8c6302
Oriol Angelet <uriaeri@gmail.com>
2024-04-09 19:09:13 +0200

    Scoreboard with stub finished

--- Memorize/Memorize/Core/View/Scoreboard.swift
+++ Memorize/Memorize/Core/View/Scoreboard.swift
@@ -12,15 +12,42 @@ struct Scoreboard: View {
     let scores = Scorecard.stub
     
     var body: some View {
-        Grid(horizontalSpacing: 500, verticalSpacing: 20) {
-            GridRow {
-                titlesRow
+        ScrollView {
+            Grid(alignment: .leadingFirstTextBaseline,
+                 horizontalSpacing: 15,
+                 verticalSpacing: 10) {
+                GridRow {
+                    Text("PLAYER")
+                    Text("DECK")
+                        .gridColumnAlignment(.center)
+                    Text("MATCHES")
+                        .gridColumnAlignment(.center)
+                    Text("SCORE")
+                        .gridColumnAlignment(.trailing)
+                }
+                .font(.callout)
+                
+                Divider()
+                    .frame(minHeight: 3)
+                    .overlay(Color.black)
+                    .padding(.horizontal, -25)
+                
+                ForEach(scores) { score in
+                    GridRow(alignment: .center) {
+                        Text(score.player)
+                        Text(score.deck)
+                            .gridColumnAlignment(.center)
+                        Text("\(score.matches)")
+                            .gridColumnAlignment(.center)
+                        Text("\(score.score)")
+                            .gridColumnAlignment(.trailing)
+                    }
+                    Divider()
+                }
+                
             }
-            Divider()
-                .frame(minHeight: 3)
-                .overlay(Color.black)
-            scoreList
         }
+        .padding(.horizontal)
     }
 }
 
@@ -30,39 +57,6 @@ struct Scoreboard: View {
 
 extension Scoreboard {
     
-    private var titlesRow: some View {
-        HStack {
-            Text("PLAYER")
-                .background(Color.blue)
-            Spacer()
-            Text("DECK")
-                .background(Color.red)
-            Spacer()
-            Text("MATCHES")
-                .background(Color.yellow)
-            Spacer()
-            Text("SCORE")
-                .background(Color.green)
-        }
-        .padding(.horizontal)
-    }
-    
-    private var scoreList: some View {
-        ForEach(scores) { score in
-            HStack {
-                Text(score.player)
-                Spacer()
-                Text(score.deck)
-                Spacer()
-                Text("\(score.matches)")
-                Spacer()
-                Text("\(score.score)")
-                Spacer()
-            }
-            .padding(.horizontal)
-            Divider()
-                .frame(minHeight: 1)
-                .overlay(Color.black)
-        }
-    }
 }
+
+
