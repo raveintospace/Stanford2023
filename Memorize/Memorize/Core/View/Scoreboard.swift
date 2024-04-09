70d202f54004d2e01f1390fba8db69b7c5604b80
Oriol Angelet <uriaeri@gmail.com>
2024-04-09 19:23:17 +0200

    Scoreboard refactored to NavigationStack to hold a navtitle

--- Memorize/Memorize/Core/View/Scoreboard.swift
+++ Memorize/Memorize/Core/View/Scoreboard.swift
@@ -14,44 +14,51 @@ struct Scoreboard: View {
     let scores = Scorecard.stub // update with real data
     
     var body: some View {
-        ScrollView {
-            Grid(alignment: .leadingFirstTextBaseline,
-                 horizontalSpacing: 20,
-                 verticalSpacing: 10) {
-                GridRow {
-                    Text("PLAYER")
-                    Text("DECK")
-                        .gridColumnAlignment(.center)
-                    Text("MATCHES")
-                        .gridColumnAlignment(.center)
-                    Text("SCORE")
-                        .gridColumnAlignment(.trailing)
-                }
-                .font(.callout)
-                
-                Divider()
-                    .frame(minHeight: 3)
-                    .overlay(viewModel.color)
-                
-                ForEach(scores) { score in
-                    GridRow(alignment: .center) {
-                        Text(score.player)
-                        Text(score.deck)
-                            .gridColumnAlignment(.center)
-                            .multilineTextAlignment(.center)
-                        Text("\(score.matches)")
-                            .gridColumnAlignment(.center)
-                        Text("\(score.score)")
-                            .gridColumnAlignment(.trailing)
+        NavigationStack {
+            ZStack {
+                ScrollView {
+                    Grid(alignment: .leadingFirstTextBaseline,
+                         horizontalSpacing: 20,
+                         verticalSpacing: 10) {
+                        GridRow {
+                            Text("PLAYER")
+                            Text("DECK")
+                                .gridColumnAlignment(.center)
+                            Text("MATCHES")
+                                .gridColumnAlignment(.center)
+                            Text("SCORE")
+                                .gridColumnAlignment(.trailing)
+                        }
+                        .font(.callout)
+                        
+                        Divider()
+                            .frame(minHeight: 3)
+                            .overlay(viewModel.color)
+                        
+                        ForEach(scores) { score in
+                            GridRow(alignment: .center) {
+                                Text(score.player)
+                                Text(score.deck)
+                                    .gridColumnAlignment(.center)
+                                    .multilineTextAlignment(.center)
+                                Text("\(score.matches)")
+                                    .gridColumnAlignment(.center)
+                                Text("\(score.score)")
+                                    .gridColumnAlignment(.trailing)
+                            }
+                            Divider()
+                                .frame(minHeight: 1)
+                                .overlay(viewModel.color.opacity(0.5))
+                        }
+                        
                     }
-                    Divider()
-                        .frame(minHeight: 1)
-                        .overlay(viewModel.color.opacity(0.5))
                 }
-                
+                .padding(.horizontal)
+                .padding(.top, 20)
             }
+            .navigationTitle("Scoreboard")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding(.horizontal)
     }
 }
 
