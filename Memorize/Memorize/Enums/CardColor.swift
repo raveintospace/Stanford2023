bb9b729462ad0f6e5c610bf3f58c070561903a53
Oriol Angelet <uriaeri@gmail.com>
2024-04-12 18:01:53 +0200

    Created extension for Color Added custom colors to enum CardColor

--- Memorize/Memorize/Enums/CardColor.swift
+++ Memorize/Memorize/Enums/CardColor.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 enum CardColor: CaseIterable, CustomStringConvertible {
     
-    case blue, brown, green, indigo, orange, purple, red, yellow
+    case blue, brown, gold, green, indigo, magenta, orange, purple, red, silver, uv, yellow
     
     var color: Color {
         switch self {
@@ -18,16 +18,24 @@ enum CardColor: CaseIterable, CustomStringConvertible {
             return Color.blue
         case .brown:
             return Color.brown
+        case .gold:
+            return Color.deckColor.gold
         case .green:
             return Color.green
         case .indigo:
             return Color.indigo
+        case .magenta:
+            return Color.deckColor.magenta
         case .orange:
             return Color.orange
         case .purple:
             return Color.purple
         case .red:
             return Color.red
+        case .silver:
+            return Color.deckColor.silver
+        case .uv:
+            return Color.deckColor.uv
         case .yellow:
             return Color.yellow
         }
@@ -39,16 +47,24 @@ enum CardColor: CaseIterable, CustomStringConvertible {
             return NSLocalizedString("Blue", comment: "")
         case .brown:
             return NSLocalizedString("Brown", comment: "")
+        case .gold:
+            return NSLocalizedString("Gold", comment: "")
         case .green:
             return NSLocalizedString("Green", comment: "")
         case .indigo:
             return NSLocalizedString("Indigo", comment: "")
+        case .magenta:
+            return NSLocalizedString("Magenta", comment: "")
         case .orange:
             return NSLocalizedString("Orange", comment: "")
         case .purple:
             return NSLocalizedString("Purple", comment: "")
         case .red:
             return NSLocalizedString("Red", comment: "")
+        case .silver:
+            return NSLocalizedString("Silver", comment: "")
+        case .uv:
+            return NSLocalizedString("UV", comment: "")
         case .yellow:
             return NSLocalizedString("Yellow", comment: "")
         }
---
+++ Memorize/Memorize/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Memorize
+//
+//  Created by Uri on 12/4/24.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let deckColor = DeckTheme()
+}
+
+struct DeckTheme {
+    
+    let gold = Color("Gold")
+    let silver = Color("Silver")
+    let uv = Color("UV")
+    let magenta = Color("Magenta")
+}
