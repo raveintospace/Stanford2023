83027708ec8cf6576621b57b6b41507a7e129c98
Oriol Angelet <uriaeri@gmail.com>
2024-04-15 18:20:16 +0200

    ConfirmationRectangle view added to project

---
+++ Memorize/Memorize/Core/Components/ConfirmationRectangle.swift
@@ -0,0 +1,35 @@
+//
+//  ConfirmationRectangle.swift
+//  Memorize
+//
+//  Created by Uri on 15/4/24.
+//
+
+import SwiftUI
+
+struct ConfirmationRectangle: View {
+    
+    let copy: String
+    let iconName: String
+    
+    var body: some View {
+        Rectangle()
+            .ignoresSafeArea()
+            .frame(height: 75)
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(Color.green).opacity(0.9)
+            .overlay(
+                HStack(spacing: 2) {
+                    Text(copy)
+                    Image(systemName: iconName)
+                }
+                .foregroundStyle(Color.white)
+                .bold()
+                .font(.system(size: 18))
+            )
+    }
+}
+
+#Preview {
+    ConfirmationRectangle(copy: "Score saved", iconName: "checkmark.seal")
+}
