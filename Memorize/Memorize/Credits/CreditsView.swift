e588eba7d79688828634b3dcf39a95064328ed07
Oriol Angelet <uriaeri@gmail.com>
2024-04-19 18:55:45 +0200

    CreditsView

---
+++ Memorize/Memorize/Credits/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  Memorize
+//
+//  Created by Uri on 19/4/24.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
