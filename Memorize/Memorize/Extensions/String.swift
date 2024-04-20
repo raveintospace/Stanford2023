5d0fb9c18153376d01041b30aec98381d4b69f2f
Oriol Angelet <uriaeri@gmail.com>
2024-04-20 18:57:46 +0200

    Removed extension for string to check duplicated characters

--- Memorize/Memorize/Extensions/String.swift
+++ Memorize/Memorize/Extensions/String.swift
@@ -9,23 +9,6 @@ import Foundation
 
 extension String {
     
-    // removes any duplicate Characters
-    // preserves the order of the Characters
-    var uniqued: String {
-        // not super efficient
-        // would only want to use it on small(ish) strings
-        // and we wouldn't want to call it in a tight loop or something
-        reduce(into: "") { sofar, element in
-            if !sofar.contains(element) {
-                sofar.append(element)
-            }
-        }
-    }
-    
-    mutating func remove(_ ch: Character) {
-        removeAll(where: { $0 == ch })
-    }
-    
     func isEmoji() -> Bool {
         for scalar in unicodeScalars {
             switch scalar.value {
