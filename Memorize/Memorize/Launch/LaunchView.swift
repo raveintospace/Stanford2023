54ced3d995e25950433256f031353fc8ce890b0d
Oriol Angelet <uriaeri@gmail.com>
2024-05-19 17:14:19 +0200

    LaunchView card iteration

--- Memorize/Memorize/Launch/LaunchView.swift
+++ Memorize/Memorize/Launch/LaunchView.swift
@@ -12,17 +12,55 @@ struct LaunchView: View {
     @Binding var showLaunchView: Bool
     
     var body: some View {
-        Text("Launch view")
-            .onAppear {
-                Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
-                    withAnimation(.easeOut(duration: 0.3)) {
-                        showLaunchView = false
-                    }
+        VStack {
+            HStack {
+                launchCard
+                launchCard
+            }
+            HStack {
+                launchCard
+                launchCard
+            }
+            HStack {
+                launchCard
+                launchCard
+            }
+            HStack {
+                launchCard
+                launchCard
+            }
+        }
+        .padding()
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+                withAnimation(.easeOut(duration: 0.3)) {
+                    showLaunchView = false
                 }
             }
+        }
     }
 }
 
 #Preview {
     LaunchView(showLaunchView: .constant(true))
+}
+
+extension LaunchView {
+    
+    private var launchCard: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .stroke(.orange, lineWidth: 2)
+            .fill()
+            .foregroundStyle(Color.white)
+            .overlay {
+                Circle()
+                    .foregroundStyle(Color.orange)
+                    .opacity(0.8)
+                    .padding(5)
+                Text("M")
+                    .font(.system(size: 50))
+            }
+            .frame(width: 100, height: 175)
+            .padding(.horizontal, 10)
+    }
 }
