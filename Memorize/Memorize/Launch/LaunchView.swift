684cc6104bc0f7bfb1b99f27b5e3c07151ae2f66
Oriol Angelet <uriaeri@gmail.com>
2024-05-20 15:36:35 +0200

    Animated text's opacity of LaunchView

--- Memorize/Memorize/Launch/LaunchView.swift
+++ Memorize/Memorize/Launch/LaunchView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LaunchView: View {
     
     @Binding var showLaunchView: Bool
+    @State private var textOpacity: Double = 0
     
     var body: some View {
         VStack {
@@ -29,9 +30,7 @@ struct LaunchView: View {
                 LaunchCard(letter: "J", startRotation: 2)
                 LaunchCard(letter: "I", startRotation: 2)
             }
-            Text("Created by Uri46")
-                .foregroundStyle(Color.orange)
-                .bold()
+            createdByText
         }
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
@@ -45,4 +44,21 @@ struct LaunchView: View {
 
 #Preview {
     LaunchView(showLaunchView: .constant(true))
+}
+
+extension LaunchView {
+    
+    private var createdByText: some View {
+        Text("Created by Uri46")
+            .foregroundStyle(Color.orange)
+            .bold()
+            .opacity(textOpacity)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    withAnimation(.easeOut(duration: 0.3)) {
+                        textOpacity = 1
+                    }
+                }
+            }
+    }
 }
